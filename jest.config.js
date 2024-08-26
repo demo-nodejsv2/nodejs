e4707bd48fc573e3e16eb6c443abf191c278efa3
pedrofnseca <pedropereira.pp264@gmail.com>
2024-08-25 21:01:39 -0300

    feat: projeto certo

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rootDir: './',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  moduleFileExtensions: ['js', 'json'],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: [],
+};
